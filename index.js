e6cad0f2a12ac2a72d2615371566136733a3a4c2
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-09-06 22:09:20 +0200

    v.0.1.3

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const express = require('express')
 const navigation1 = require('./navigation1.json')
 const navigation2 = require('./navigation2.json')
 const pizzas = require('./pizzas.json')
+// const carousel = require('react-multi-carousel')
+// const style = require(`"react-multi-carousel/lib/styles.css"`)
+
+// const responsive = {
+//     desktop: {
+//       breakpoint: { max: 3000, min: 1024 },
+//       items: 3,
+//       slidesToSlide: 3 // optional, default to 1.
+//     },
+//     tablet: {
+//       breakpoint: { max: 1024, min: 464 },
+//       items: 2,
+//       slidesToSlide: 2 // optional, default to 1.
+//     },
+//     mobile: {
+//       breakpoint: { max: 464, min: 0 },
+//       items: 1,
+//       slidesToSlide: 1 // optional, default to 1.
+//     }
+//   };
 
 const app = express()
 app.listen(3000, (req, res) => {
